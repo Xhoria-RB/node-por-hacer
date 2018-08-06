8d6282d035db9e1bf416d8a1d0211a69c8dd1bc9
Ricardo Bibieca <shirotokuro3715@gmail.com>
2018-08-06 16:15:54 -0400

    Primer commit

---
+++ config/yargs.js
@@ -0,0 +1,29 @@
+const descripcion = {
+    alias: 'd',
+    demand: true,
+    desc: 'Descripcion de la tarea por hacer'
+};
+
+const completado = {
+    alias: 'c',
+    default: true,
+    desc: 'Marca como completado o pendiente la tarea'
+};
+
+const argv = require('yargs')
+    .command('crear', 'Crea una tarea por hacer', {
+        descripcion //descripcion: descripcion
+    })
+    .command('actualizar', 'Actualiza una tarea por hacer', {
+        descripcion,
+        completado
+    })
+    .command('borrar', 'Borra una tarea por hacer', {
+        descripcion
+    })
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+};
